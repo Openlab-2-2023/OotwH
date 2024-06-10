563b5e3a81aadf30457d52877414f925ddddfdae
Dejvidko <davidbudos699@gmail.com>
2024-06-10 21:44:12 +0200

    commit

---
+++ docs/scripts.js
@@ -0,0 +1,14 @@
+
+[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]
+
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    const loadingScreen = document.getElementById('loadingScreen');
+    const mainContent = document.getElementById('mainContent');
+    
+    // Simulate loading process
+    setTimeout(() => {
+        loadingScreen.style.display = 'none';
+        mainContent.style.display = 'block';
+    }, 3000); // Adjust the timeout duration as needed
+});
